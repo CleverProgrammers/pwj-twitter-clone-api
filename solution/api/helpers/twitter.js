060231f6b374f7055aac705286fed46e90797b66
Nazariy995 <nazariy1995@gmail.com>
2020-03-03 18:22:58 -0800

    ADD: max id

--- solution/api/helpers/twitter.js
+++ solution/api/helpers/twitter.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 const TWITTER_URL = "https://api.twitter.com/1.1/search/tweets.json";
 
 class Twitter {
-    get(query, resultType) {
+    get(query, resultType, maxId) {
         return axios.get(TWITTER_URL, {
             params: {
                 q: query,
                 result_type: resultType,
                 count: 10,
-                tweet_mode: "extended"
+                tweet_mode: "extended",
+                max_id: maxId
             },
             headers: {
                 "Authorization": `Bearer ${process.env.TWITTER_API_TOKEN}`
--- solution/app.js
+++ solution/app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.get('/tweets', (req, res)=>{
     const query = req.query.q;
     const resultType = req.query.result_type;
-    twitter.get(query, resultType).then((response)=>{
+    const maxId = req.query.max_id;
+    twitter.get(query, resultType, maxId).then((response)=>{
         res.status(200).send(response.data);
     }).catch((error)=>{
         res.status(400).send(error);
